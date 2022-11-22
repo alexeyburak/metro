a651ef0aa41c52f2a97af1260a1c54abeb26b9d5
Alexey Burak <byraklesha@gmail.com>
2022-11-22 21:05:49 +0300

    refactor: add home page template

--- src/main/java/com/burak/metro/config/MvcConfig.java
+++ src/main/java/com/burak/metro/config/MvcConfig.java
@@ -16,5 +16,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("main-window");
     }
 }
